35641345d3c6fb6cfe30b6541dbae8904bed50c0
Kiril <kiril2315@gmail.com>
2026-01-10 00:14:05 +0200

    debug fetching

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,9 +6,6 @@ import { loadEnv } from "vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
-  console.log("DEBUG import.meta:", import.meta);
-  console.log("DEBUG env from loadEnv:", env);
-
   return {
     plugins: [vue()],
     server: {
@@ -16,7 +13,8 @@ export default defineConfig(({ mode }) => {
         "/api": {
           target: env.VITE_API_URL || "http://localhost:3001",
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
+          rewrite: (path) =>
+            env.VITE_API_URL + "/" + path.replace(/^\/api/, ""),
         },
       },
       host: true,
